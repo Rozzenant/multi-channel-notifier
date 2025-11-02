c07cfa1298c52ba3dfaae5a07b8a9e7f24dc9143
Rozzenant <dmitry.pe4urckin@yandex.ru>
2025-11-02 20:15:36 +0300

    Created Notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Notification(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "В ожидании"),
+        ("sent", "Отправлено"),
+        ("failed", "Ошибка"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    channels_tried = models.JSONField(default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
